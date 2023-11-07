86f0d508af8c309c53545d573b4a476c15f8d9e5
Gustavo Pina <115650988+GuPina@users.noreply.github.com>
2023-11-06 21:59:37 -0300

    router

---
+++ src/core/router.py
@@ -0,0 +1,24 @@
+from fastapi import APIRouter
+
+from .config import get_settings
+from .logger import ApiLogger
+
+router = APIRouter()
+
+@router.get('/')
+async def root():
+    settings = get_settings()
+    api_logger = ApiLogger()
+
+    api_logger.debug('This is a debug line')
+    api_logger.info('This is a info line')
+    api_logger.warning('This is a warn line')
+    api_logger.error('This is a error line')
+    api_logger.critical('This is a critical line')
+
+    return {'message': 'Hello World', 'log_level': settings.log_level}
+
+@router.post('/teste-2')
+async def root():
+    return {'message': 'Hello World222'}
+
--- src/main.py
+++ src/main.py
@@ -1,23 +1,7 @@
 from fastapi import FastAPI
-from src.core.config import get_settings
-from src.core.logger import ApiLogger
+from src.core.router import router
+
 
 app = FastAPI()
 
-@app.get('/')
-async def root():
-    settings = get_settings()
-    api_logger = ApiLogger()
-
-    api_logger.debug('This is a debug line')
-    api_logger.info('This is a info line')
-    api_logger.warning('This is a warning line')
-    api_logger.error('This is a error line')
-    api_logger.critical('This is a critical line')
-
-    return {'message': 'Hello World', 'log_level': settings.log_level}
-
-@app.post('/teste-2')
-async def root():
-    return {'message': 'Hello World222'}
-
+app.include_router(router, prefix='/api')
