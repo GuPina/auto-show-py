3c3e52c30dd651e953610448eca02d454ad86ebd
Gustavo Pina <115650988+GuPina@users.noreply.github.com>
2023-11-06 21:41:12 -0300

    logger

--- src/main.py
+++ src/main.py
@@ -1,11 +1,20 @@
 from fastapi import FastAPI
 from src.core.config import get_settings
+from src.core.logger import ApiLogger
 
 app = FastAPI()
 
 @app.get('/')
 async def root():
     settings = get_settings()
+    api_logger = ApiLogger()
+
+    api_logger.debug('This is a debug line')
+    api_logger.info('This is a info line')
+    api_logger.warning('This is a warning line')
+    api_logger.error('This is a error line')
+    api_logger.critical('This is a critical line')
+
     return {'message': 'Hello World', 'log_level': settings.log_level}
 
 @app.post('/teste-2')
